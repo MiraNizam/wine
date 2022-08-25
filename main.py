03fff1fe43f30e6d1b7b53499ee4fb04e6e4ec43
Эльмира Низамова <e.nizamova@integra.local>
2022-08-25 17:12:39 +0300

    changes to 10th step

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+#from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import  datetime
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def wine_age():
+    start_year = 1920
+    current_year = datetime.datetime.today().year
+    return current_year - start_year
+
+
+def correct_year():
+    year = wine_age()
+    remainder = year % 100
+    if remainder == 0 or remainder in range(5, 21):
+        return "лет"
+    elif remainder == 1:
+        return "год"
+    else:
+        return "года"
+
+
+rendered_page = template.render(age=wine_age(), year=correct_year())
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+#server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+#server.serve_forever()
